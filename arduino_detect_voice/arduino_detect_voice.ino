e55bc01680bd34e8dd80a5a76a2178170a756fbf
BaoBang <baobangb5@gmail.com>
2018-04-24 17:30:15 +0700

    finshed

--- arduino_detect_voice/arduino_detect_voice.ino
+++ arduino_detect_voice/arduino_detect_voice.ino
@@ -1,22 +1,10 @@
-
-#include <SoftwareSerial.h>
-// define SoftwareSerial rx data pin
-#define rxPin 0
-// define SoftwareSerial tx data pin                        
-#define txPin 1                     
 String data;
-int LED = 13; 
-int sensorPin = A0;
-int sensorValue = 0;
-// create instance of SoftwareSerial
-SoftwareSerial blueTooth(rxPin, txPin); 
+int LED = 13;
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
- blueTooth.begin(9600);
  pinMode(LED, OUTPUT);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
   while (Serial.available()){  //Kiểm tra byte để đọc
@@ -27,15 +15,11 @@ void loop() {
   } 
   if (data.length() > 0) {
       Serial.println(data);  
-      if(data == "bật đèn" ||data == "Bật đèn"||data == "Bật Đèn" 
-        || data == "on" || data == "On" || data == "ON"){
+      if(data == "on"){
         digitalWrite(LED, HIGH);
-        Serial.flush();
         Serial.write("1");
-      }else if(data == "tắt đèn"||data == "Tắt đèn"||data == "Tắt Đèn"
-          || data == "off" || data == "Off" || data == "OFF"){
+      }else if(data == "off"){
         digitalWrite(LED, LOW);
-        Serial.flush();
         Serial.write("0");
       }
   }
